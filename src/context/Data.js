cda0fb473ef0c952d2688a6e4b1b6663c56751a1
Umair Shahid <umair.shahid0088@gmail.com>
2021-01-18 14:37:45 +0500

    update pics

--- src/context/Data.js
+++ src/context/Data.js
@@ -1,28 +1,28 @@
-export const shoes={
-    "NIke":{
-            name:"Nike",
-            img:"https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=mobile_hero",
-            price:200,
-            details:"ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
-          },
-    "Puma":{
-            name:"Puma",
-            img:"https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=mobile_hero",
-            price:200,
-            details:"ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
-          },
-    "Bata":{
-            name:"Bata",
-            img:"https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=mobile_hero",
-            price:200,
-            details:"ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
-            },
-    "Service":{
-            name:"Service",
-            img:"https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=mobile_hero",
-            price:200,
-            details:"ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
-            }
+export const shoes = {
+        "NIke": {
+                name: "Fila",
+                img: "https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=mobile_hero",
+                price: 200,
+                details: "ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
+        },
+        "Puma": {
+                name: "Puma",
+                img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/449c838942da409f8ba9a97f00d3cffe_9366/Runfalcon_Shoes_Black_F36199_01_standard.jpg",
+                price: 200,
+                details: "ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
+        },
+        "Bata": {
+                name: "Bata",
+                img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f77bea97de7548068afca9ae0164d5d6_9366/Runfalcon_Shoes_Black_G28970_01_standard.jpg",
+                price: 200,
+                details: "ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
+        },
+        "Service": {
+                name: "Service",
+                img: "https://lh3.googleusercontent.com/proxy/iNt5BdUHVPyym8spspWl9xmonYmhRLZDy4rdJufkdu4lihpT09MFtLVBMLXKIPI1jmKvlBO1gEZqkpOMEs1SI16dUPeJV7KGaGrPekR1jDwZ8JpVxSMX1xuRarzLZD9g30guFETGrcqvbUVBIFGG6bHd2buEaD0tno4xW5R67Q",
+                price: 200,
+                details: "ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained"
+        }
 
 
 }
